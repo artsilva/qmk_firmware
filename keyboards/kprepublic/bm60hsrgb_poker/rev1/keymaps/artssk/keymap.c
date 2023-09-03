01d48c699443c0f384fddb849bcf29a192191075
asilvar <asilvar@internacional.cl>
2023-09-03 20:47:01 -0300

    se elimina sufijo de tapdance

--- keyboards/kprepublic/bm60hsrgb_poker/rev1/keymaps/artssk/keymap.c
+++ keyboards/kprepublic/bm60hsrgb_poker/rev1/keymaps/artssk/keymap.c
@@ -21,7 +21,7 @@ enum {
   TD_ESC_CAPS = 0
 };
 
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Kc grv, twice for Esc
   [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
 // Other declarations would go here, separated by commas, if you have them
