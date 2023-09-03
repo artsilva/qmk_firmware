45e6658d52f28d024ca790736604641952ff1b21
asilvar <asilvar@internacional.cl>
2023-09-03 20:39:39 -0300

    tapping term configurations

--- keyboards/kprepublic/bm60hsrgb_poker/rev1/rules.mk
+++ keyboards/kprepublic/bm60hsrgb_poker/rev1/rules.mk
@@ -11,3 +11,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
+TAP_DANCE_ENABLE = yes
